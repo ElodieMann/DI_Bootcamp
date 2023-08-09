6da4cd73db005393f311e71153986401321f8878
ElodieMann <elodie.el1409@gmail.com>
2023-08-09 22:19:54 +0300

    fix daily challenge

--- Week4/Day4/DailyChallenge/index.js
+++ Week4/Day4/DailyChallenge/index.js
@@ -1,28 +1,30 @@
 let counter = 1;
-let bottles = prompt("Choose a number");
-
+let bottles = parseInt(prompt("Choose a number"), 10);
 
 console.log(`We start the song at ${bottles} bottles`);
 do {
-    let bottleWord;
-    let sing;
-    if (bottles < 2) {
-      bottleWord = "bottle";
-    } else {
-      bottleWord = "bottles"
-    }
+  let bottleWord;
+  let sing;
+  if (bottles < 2) {
+    bottleWord = "bottle";
+  } else {
+    bottleWord = "bottles";
+  }
+
+  if (counter < 2) {
+    sing = "it";
+  } else {
+    sing = "them";
+  }
+  bottles = bottles - counter;
+
+  let text1 = `Take ${counter} down, pass ${sing} around`;
+  let text2 = `we have now ${bottles} ${bottleWord}`;
   
-    if (counter < 2) {
-      sing = "it";
-    } else {
-      sing = "them"
-    }
-  
-    let text1 = `Take ${counter} down, pass ${sing} around`;
-    let text2 = `we have now ${bottles - 1} ${bottleWord}`;
-  
-    bottles = bottles - (counter + 1);
-    counter++;
-  
-    console.log(text1, text2);
-} while (bottles >= 0 )
+  counter++;
+  if (counter > bottles) {
+     (counter = bottles);
+  }
+  console.log(text1, text2);
+} while (bottles > 0);
+console.log('No bottle of beer on the wall');
